519a0c7f496b2235bc4a2e863ee4b2e122776171
andrei1010 <andrei@andrei1010.me>
2025-11-11 15:20:42 +0000

    decrypt

--- src/utils/auth/session.ts
+++ src/utils/auth/session.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/utils/db'
 import { CompactEncrypt, compactDecrypt } from "jose";
 import crypto from 'crypto';
+import { resolve } from 'path';
 
 const SESSION_TTL_MS = 1000 * 60 * 60 * 24 * 2;
 const expires = new Date(Date.now() + SESSION_TTL_MS);
@@ -44,5 +45,22 @@ export async function generateSessionCookie(sessionId: string): Promise<string> 
       .setProtectedHeader({ alg: 'dir', enc: 'A256GCM' })
       .encrypt(secret);
     resolve(jwe);
+  })
+}
+
+export async function decryptSessionCookie(sessionCookie: string): Promise<{ sessionId: string; exp: string }> {
+  return new Promise(async (resolve, reject) => {
+    if (!process.env.SECRET) {
+      reject(new Error('process.env.SECRET is missing'));
+    }
+    
+    const secret = crypto.createHash('sha256')
+      .update(process.env.SECRET as string)
+      .digest();
+    
+    const { plaintext } = await compactDecrypt(sessionCookie, secret);
+    const decoded = new TextDecoder().decode(plaintext);
+    const sessionData = JSON.parse(decoded);
+    resolve(sessionData);
   })
 }
