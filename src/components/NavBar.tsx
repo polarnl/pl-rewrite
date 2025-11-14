740784b5d7699a698bc2647f9e72803e148e0e4e
Jesse Hoekema <109305882+JesseHoekema@users.noreply.github.com>
2025-11-14 17:43:29 +0100

    fixed padding of menubar content on smaller devices

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ export function PolarNavBar() {
   return (
     <>
       <nav className="fixed inset-x-0 top-0 z-50 max-h-16 h-full border-b border-neutral-800/60 bg-neutral-900/80 backdrop-blur">
-        <div className="mx-auto flex h-full w-full max-w-7xl items-center justify-between">
+        <div className="flex h-full w-full items-center justify-between">
           <Image
             src="/icon.svg"
             width={50}
